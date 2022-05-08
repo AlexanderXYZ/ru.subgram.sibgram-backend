a64d31ae7970754dec72eb4c39c52c9275f74884
Alexander <goode321123@gmail.com>
2022-05-08 14:52:21 +0700

    Initial commit

---
+++ src/main/kotlin/ru/subgram/Application.kt
@@ -0,0 +1,16 @@
+package ru.subgram
+
+import io.ktor.server.application.*
+import ru.subgram.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureRouting()
+    configureSockets()
+    configureSerialization()
+    configureMonitoring()
+    configureSecurity()
+}
